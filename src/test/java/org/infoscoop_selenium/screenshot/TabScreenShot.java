5abe0389266c5343c365bfb31185e26c3335c8f5
mikami <mikami@beacon-it.co.jp>
2013-02-21 19:11:02 +0900

    タブ関連のスクリーンショット

--- src/test/java/org/infoscoop_selenium/screenshot/TabScreenShot.java
+++ src/test/java/org/infoscoop_selenium/screenshot/TabScreenShot.java
@@ -34,10 +34,76 @@ public class TabScreenShot extends IS_BaseItTestCase{
 		WebDriver driver = getDriver();
 		
 		// login
-		getPortal().login("test_user1", "password");
-
+		getPortal().login("test_user2", "password");
+		
+		// 初期化
+		getPortal().getCommandBar().getPortalPreference().initializeData();
+		
+		// タブメニューボタンの表示を待つ
+		TestHelper.waitPresent(driver, By.id("tab0_selectMenu"));
+		driver.findElement(By.id("tab0_selectMenu")).click();
+		
+		// タブメニューの表示を待つ
+		TestHelper.waitPresent(driver, By.id("tab0_menu"));
+		
 		TestHelper.getScreenShot("タブメニュー（固定タブ）", getDriver());
 
+		assertTrue(true);
+	}
+	
+	@Test
+	/**
+	 * タブメニュー（動的タブ）
+	 */
+	public void タブメニュー_動的タブ(){
+		WebDriver driver = getDriver();
+		
+		// login
+		getPortal().login("test_user2", "password");
+		
+		// 初期化
+		getPortal().getCommandBar().getPortalPreference().initializeData();
+		
+		// タブを追加するリンクの表示を待つ
+		TestHelper.waitPresent(driver, By.id("addTab"));
+		driver.findElement(By.xpath("//div[@id='addTab']/a")).click();
+		
+		// 動的タブの作成を待つ
+		TestHelper.waitPresent(driver, By.id("tab1"));
+		driver.findElement(By.id("tab1_selectMenu")).click();
+		
+		// タブメニューの表示を待つ
+		TestHelper.waitPresent(driver, By.id("tab1_menu"));
+
+		TestHelper.getScreenShot("タブメニュー（動的タブ）", getDriver());
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	/**
+	 * 画面横幅を超えた際の折り返し
+	 */
+	public void 画面横幅を超えた際の折り返し(){
+		WebDriver driver = getDriver();
+		
+		// login
+		getPortal().login("test_user2", "password");
+		
+		// 初期化
+		getPortal().getCommandBar().getPortalPreference().initializeData();
+		
+		// タブを追加するリンクの表示を待つ
+		TestHelper.waitPresent(driver, By.id("addTab"));
+		
+		// 動的タブを作成する
+		for(int i=1;i<30;i++){
+			driver.findElement(By.xpath("//div[@id='addTab']/a")).click();
+			TestHelper.waitPresent(driver, By.id("tab"+i));
+		}
+		
+		TestHelper.getScreenShot("画面横幅を超えた際の折り返し", getDriver());
+		
 		assertTrue(true);
 	}
 	
