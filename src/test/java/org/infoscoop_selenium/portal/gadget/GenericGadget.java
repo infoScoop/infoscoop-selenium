49b83709573ebc47b2466f296ed15da6fab66891
nishiumi <nishiumi@beacon-it.co.jp>
2013-02-26 17:50:41 +0900

    リファクタリング

---
+++ src/test/java/org/infoscoop_selenium/portal/gadget/GenericGadget.java
@@ -0,0 +1,13 @@
+package org.infoscoop_selenium.portal.gadget;
+
+import org.infoscoop_selenium.portal.Gadget;
+import org.openqa.selenium.WebDriver;
+
+public class GenericGadget extends Gadget{
+
+	public GenericGadget(WebDriver driver, String gadgetId) {
+		super(driver, gadgetId);
+		// TODO Auto-generated constructor stub
+	}
+
+}
