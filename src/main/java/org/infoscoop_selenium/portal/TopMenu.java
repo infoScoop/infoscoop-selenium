2d10d7f797209d62a6e7ab3f048d9b9e0b8f48e4
b1733 <b1733@P01102.bsp.co.jp>
2014-06-17 18:02:52 +0900

    トップメニュークラス修正

--- src/main/java/org/infoscoop_selenium/portal/TopMenu.java
+++ src/main/java/org/infoscoop_selenium/portal/TopMenu.java
@@ -1,5 +1,7 @@
 package org.infoscoop_selenium.portal;
 
+import java.util.List;
+
 import org.infoscoop_selenium.helper.TestHelper;
 import org.infoscoop_selenium.portal.Gadget.GADGET_TYPE;
 import org.openqa.selenium.By;
@@ -12,24 +14,25 @@ import org.openqa.selenium.interactions.Actions;
 
 public class TopMenu {
 	WebDriver driver;
-	
+
 	public TopMenu(WebDriver driver) {
 		this.driver = driver;
 	}
-	
+
 	/**
 	 * 指定IDのトップメニュー開く
 	 */
 	public void openTopMenu(String menuId){
 		TestHelper.waitPresent(driver, By.id(menuId));
-		
+
 		// トップメニューにマウスオーバー
 		Actions action = new Actions(driver);
 //		action.moveToElement(driver.findElement(By.id(menuId))).click().build().perform();
 		action.moveToElement(driver.findElement(By.id(menuId))).build().perform();
 		TestHelper.sleep(1000);
-		
-		TestHelper.waitPresent(driver, By.xpath("//li[@id='" + menuId + "']//ul[1]"));
+
+//		TestHelper.waitPresent(driver, By.xpath("//li[@id='" + menuId + "']//ul[1]"));
+		TestHelper.waitPresent(driver, By.cssSelector("#" + menuId + " .menuGroup"));
 	}
 
 	/**
@@ -47,24 +50,35 @@ public class TopMenu {
 	public Gadget dropGadget(String parentId, String id, int columnNum, GADGET_TYPE gadgetType){
 		return dropGadget(parentId, id, columnNum, gadgetType, true);
 	}
-	
+
 	/**
 	 * 指定IDのガジェットをドロップする
 	 * e.g) asahi.com = news_asahi
 	 */
 	public Gadget dropGadget(String parentId, String id, int columnNum, GADGET_TYPE gadgetType, boolean dropFlg){
 		openTopMenu(parentId);
-		
-		WebElement dropElement = driver.findElement(By.xpath("//div[@class='column' and @colnum='" + columnNum + "']"));
+
+		List<WebElement> panels = driver.findElements(By.cssSelector(".panel"));
+		System.out.println(panels.size());
+		WebElement activeTabEl = null;
+		for( int i=0;i<panels.size();i++ ) {
+			WebElement el = panels.get(i);
+			System.out.println(el);
+			System.out.println(el.isDisplayed());
+			if( el.isDisplayed() )
+				activeTabEl = el;
+		}
+
+		WebElement dropElement = activeTabEl.findElement(By.xpath("//div[@class='column' and @colnum='" + columnNum + "']"));
 		Point dropPoint = dropElement.getLocation();
-		
+
 		WebElement targetElement = driver.findElement(By.id("mi_" + id));
-		
+
 		Actions actions = new Actions(driver);
 
 		// Firefox (on windows) だと固まる
 //		actions.dragAndDropBy(targetElement, dropPoint.x, dropPoint.y).perform();
-		
+
 		// IE, FFで動作するコード
 		actions.moveToElement(targetElement);
 		actions.clickAndHold();
@@ -72,25 +86,26 @@ public class TopMenu {
 		if(dropFlg)
 			actions.release();
 		actions.build().perform();
-		
+
 		if(dropFlg){
 			// FIXME: IEだとオーバーレイが残ってしまい、操作不能になる。できればWebDriverでなんとかしたい
 			((JavascriptExecutor)driver).executeScript("IS_Portal.hideDragOverlay();");
-			
+
 			// メニューが残っている場合消去する
-//			if(driver.findElement(By.xpath("//li[@id='etcWidgets']//img[@class='closeMenu']")).isDisplayed()){
-//				try{
-//					Thread.sleep(500);
-//				}catch(Exception e){
-//					throw new RuntimeException(e);
-//				}
-//				WebElement closeDiv = driver.findElement(By.xpath("//li[@id='etcWidgets']//img[@class='closeMenu']"));
-//				actions.moveToElement(closeDiv);
-//				actions.build().perform();
-//			}
-			
-			String widgetId = dropElement.findElements(By.className("widget")).get(0).getAttribute("id");
-			
+			if(driver.findElement(By.xpath("//li[@id='etcWidgets']//img[@class='closeMenu']")).isDisplayed()){
+				try{
+					Thread.sleep(500);
+				}catch(Exception e){
+					throw new RuntimeException(e);
+				}
+				WebElement closeDiv = driver.findElement(By.xpath("//li[@id='etcWidgets']//img[@class='closeMenu']"));
+				actions.moveToElement(closeDiv);
+				actions.build().perform();
+			}
+
+			System.out.println("dropElement: " + dropElement);
+			String widgetId = activeTabEl.findElements(By.className("widget")).get(0).getAttribute("id");
+
 //			return new Gadget(driver, widgetId);
 			Class<Gadget> c = gadgetType.getValue();
 			try {
@@ -119,24 +134,24 @@ public class TopMenu {
 	public Gadget dropFolder(String parentId, int columnNum, GADGET_TYPE gadgetType){
 		return dropFolder(parentId, columnNum, gadgetType, true);
 	}
-	
+
 	/**
 	 * まとめてドロップ
 	 */
 	public Gadget dropFolder(String parentId, int columnNum, GADGET_TYPE gadgetType, boolean dropFlg){
 		openTopMenu(parentId);
-		
+
 		WebElement dropElement = driver.findElement(By.xpath("//div[@class='column' and @colnum='" + columnNum + "']"));
 		Point dropPoint = dropElement.getLocation();
-		
+
 		WebElement parentElement = driver.findElement(By.id("mg_" + parentId + "_0"));
 		WebElement targetElement = parentElement.findElement(By.className("menufolderfeedTitle"));
-		
+
 		Actions actions = new Actions(driver);
 
 		// Firefox (on windows) だと固まる
 //		actions.dragAndDropBy(targetElement, dropPoint.x, dropPoint.y).perform();
-		
+
 		// IE, FFで動作するコード
 		actions.moveToElement(targetElement);
 		actions.clickAndHold();
@@ -144,11 +159,11 @@ public class TopMenu {
 		if(dropFlg)
 			actions.release();
 		actions.build().perform();
-		
+
 		if(dropFlg){
 			// FIXME: IEだとオーバーレイが残ってしまい、操作不能になる。できればWebDriverでなんとかしたい
 			((JavascriptExecutor)driver).executeScript("IS_Portal.hideDragOverlay();");
-			
+
 			// メニューが残っている場合消去する
 //			if(driver.findElement(By.xpath("//li[@id='etcWidgets']//img[@class='closeMenu']")).isDisplayed()){
 //				try{
@@ -160,9 +175,9 @@ public class TopMenu {
 //				actions.moveToElement(closeDiv);
 //				actions.build().perform();
 //			}
-			
+
 			String widgetId = dropElement.findElements(By.className("widget")).get(0).getAttribute("id");
-			
+
 //			return new Gadget(driver, widgetId);
 			Class<Gadget> c = gadgetType.getValue();
 			try {
