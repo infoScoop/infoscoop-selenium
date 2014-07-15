1c90be1e5de28e64d03c83fae2fc2f0f01656b76
hiuchida <hiuchida@beacon-it.co.jp>
2014-07-15 16:46:56 +0900

    ツール系ガジェット/標準時時計/表示

--- src/test/java/org/infoscoop_selenium/testsuites/tool_gadgets/ToolGadgets_Clock_DisplayTest.java
+++ src/test/java/org/infoscoop_selenium/testsuites/tool_gadgets/ToolGadgets_Clock_DisplayTest.java
@@ -1,5 +1,10 @@
 package org.infoscoop_selenium.testsuites.tool_gadgets;
 
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import org.infoscoop_selenium.base.IS_BaseItTestCase;
 import org.infoscoop_selenium.portal.Gadget.GADGET_TYPE;
 import org.infoscoop_selenium.portal.gadget.ClockGadget;
@@ -24,18 +29,35 @@ public class ToolGadgets_Clock_DisplayTest extends IS_BaseItTestCase {
 	@Test
 	/**
 	 * 時刻
-	 * @XXX チェック方法を検討
 	 */
-	public void iscp_4110(){
+	public void iscp_4110() throws Exception {
 		String jpDate = GADGET.getJpDate();
-		String jpTime = GADGET.getJpTime();
 		String ukDate = GADGET.getUkDate();
-		String ukTime = GADGET.getUkTime();
 		String usDate = GADGET.getUsDate();
+		String jpTime = GADGET.getJpTime();
+		String ukTime = GADGET.getUkTime();
 		String usTime = GADGET.getUsTime();
-		System.out.println(jpDate + " " + jpTime);
-		System.out.println(ukDate + " " + ukTime);
-		System.out.println(usDate + " " + usTime);
+		//JPのUNIX時間
+		SimpleDateFormat jpdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		long jpVal = jpdf.parse(jpDate + " " + jpTime).getTime();
+		//UKのUNIX時間
+		SimpleDateFormat ukdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		ukdf.setTimeZone(TimeZone.getTimeZone("Europe/London"));
+		long ukVal = ukdf.parse(ukDate + " " + ukTime).getTime();
+		//USのUNIX時間
+		SimpleDateFormat usdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		usdf.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+		long usVal = usdf.parse(usDate + " " + usTime).getTime();
+		//1秒の誤差を補正する
+		if (!jpTime.substring(6).equals(ukTime.substring(6))) {
+			ukVal -= 1000;
+		}
+		if (!jpTime.substring(6).equals(usTime.substring(6))) {
+			usVal -= 1000;
+		}
+		//タイムゾーンを考慮すると同じUNIX時間となる
+		assertEquals(jpVal, ukVal);
+		assertEquals(jpVal, usVal);
 	}
 
 	//@Test
