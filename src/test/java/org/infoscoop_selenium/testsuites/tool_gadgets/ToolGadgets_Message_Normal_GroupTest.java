b1b2bb69195bfb8eeefb714ee073cb3dedb18d60
ishizuka <ishizuka@beacon-it.co.jp>
2014-09-09 17:13:57 +0900

    iscp_4217

--- src/test/java/org/infoscoop_selenium/testsuites/tool_gadgets/ToolGadgets_Message_Normal_GroupTest.java
+++ src/test/java/org/infoscoop_selenium/testsuites/tool_gadgets/ToolGadgets_Message_Normal_GroupTest.java
@@ -7,6 +7,7 @@ import org.infoscoop_selenium.base.IS_BaseItTestCase;
 import org.infoscoop_selenium.portal.Gadget.GADGET_TYPE;
 import org.infoscoop_selenium.portal.gadget.MessageGadget;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class ToolGadgets_Message_Normal_GroupTest extends IS_BaseItTestCase {
@@ -42,7 +43,7 @@ public class ToolGadgets_Message_Normal_GroupTest extends IS_BaseItTestCase {
     public void iscp_4214() {
         String str = "①㈱㌢<>&\"'!\"#$%'()~=-^@`'*+;:[{]}\\_/?<>\\.タダイマポチゼンブアソボゾウノファミリ―～∥－￤";
 
-        GADGET.changeTabName(0, str);
+        GADGET.changeGroupName(0, str);
 
         WebElement group = GADGET.getMessageGroupElement(1);
 
@@ -60,9 +61,45 @@ public class ToolGadgets_Message_Normal_GroupTest extends IS_BaseItTestCase {
         int height = GADGET.getMessageGroupElement(1).getSize().height;
 
         // change tab name
-        GADGET.changeTabName(0, "01234567890123456789012345678901234567890123456789");
+        GADGET.changeGroupName(0, "01234567890123456789012345678901234567890123456789");
 
         assertTrue(height < GADGET.getMessageGroupElement(1).getSize().height);
+    }
+
+    //@Test
+    /**
+     * ユーザの並べ替え
+     */
+    public void iscp_4216() {
+        /*
+         * グループ編集ウィンドウで、グループ内のユーザを並べ替えた場合に、メッセージウィジェットの表示に正しく反映されることを確認。
+         */
+    }
+
+    @Test
+    /**
+     * グループの削除
+     */
+    public void iscp_4217() {
+        // add group
+        GADGET.addGroup(null);
+        
+        int groupCount = GADGET.getContentsElement().findElements(By.className("messageGroup")).size();
+        
+        // delete group
+        GADGET.deleteGroup(0);
+        
+        assertTrue(groupCount > GADGET.getContentsElement().findElements(By.className("messageGroup")).size());
+    }
+
+    //@Test
+    /**
+     * ユーザの削除
+     */
+    public void iscp_4218() {
+        /*
+         * グループ編集ウィンドウでグループ内のユーザを削除した場合、メッセージウィジェットの表示にも正しく反映されることを確認。
+         */
     }
 
 }
